aa9fe865ef6e4b49bd52fbb3ed34ecb5a4c339ac
leonid <leo_lis@mail.ru>
2020-04-27 08:13:01 +0300

    +

--- src/Encryptor/EncryptorBuilder.java
+++ src/Encryptor/EncryptorBuilder.java
@@ -7,11 +7,13 @@ public class EncryptorBuilder {
 
     public static Encryptor encryptor(EncryptorType encryptorType){
 
-        switch (encryptorType){
-            case FIRST_TYPE_ENCRYPTOR:
-                return firstTypeEncryptor;
-            case SECOND_TYPE_ENCRYPTOR:
-                return secondTypeEncryptor;
+        synchronized (EncryptorBuilder.class) {
+            switch (encryptorType) {
+                case FIRST_TYPE_ENCRYPTOR:
+                    return firstTypeEncryptor;
+                case SECOND_TYPE_ENCRYPTOR:
+                    return secondTypeEncryptor;
+            }
         }
         return null;
     }
